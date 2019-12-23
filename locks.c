e9013185c751bb00e03fbdc0e7d70978563e7124
maxiutn <13839942+maxiutn@users.noreply.github.com>
2019-12-23 15:26:59 -0300

    ser q

--- locks.c
+++ locks.c
@@ -6,11 +6,14 @@ Nadie_Te_Invito = leerArch(Arch);
 Hilo 2 (ult)
 ----
 
+
 Nadie_Te_Invito = getToken();
 escribirPropuesta(Arch);
+
 
 Hilo 3 (ult)
 ----
 
 Nadie_Te_Invito = getToken();
+
 escribirPropuesta(Arch);
